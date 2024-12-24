4ff1cf3bff36da6f683e9079beb4f9489744fdb4
Saif Maamun <sadifmamun@gmail.com>
2024-12-24 23:47:57 +0600

    why part 4

--- src/sections/why/HorizontalScroll.tsx
+++ src/sections/why/HorizontalScroll.tsx
@@ -11,10 +11,10 @@ const HorizontalScroll = () => {
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+  const x = useTransform(scrollYProgress, [0, 1.25], ["1%", "-95%"]);
 
   return (
-    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+    <section ref={targetRef} className="relative h-[200vh] bg-neutral-900">
       <div className="sticky top-0 flex  items-center overflow-hidden">
         <motion.div style={{ x }} className="flex">
           <Why1 />
